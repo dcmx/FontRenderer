a4fe033a12cf8ac153044ba6607491f8eb361f12
Dominik Kaaser <dominik@kaaser.at>
2012-08-02 12:32:23 +0200

    Fixed default line height.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -15,7 +15,7 @@
 namespace dcm {
 
 Renderer::Renderer() :
-		PRECISION(20), DEFAULTLINEHEIGHT(-1.2), lineHeight(1.0), letterSpacing(1.0), sampleRate(100), doScaleToUnit(
+		PRECISION(20), DEFAULTLINEHEIGHT(-1.8), lineHeight(1.0), letterSpacing(1.0), sampleRate(100), doScaleToUnit(
 				true) {
 
 	if (FT_Init_FreeType(&library))
